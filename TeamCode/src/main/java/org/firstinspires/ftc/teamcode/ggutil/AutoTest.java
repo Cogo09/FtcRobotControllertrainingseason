fe083ce3296ba0c39a06a4feacd65da92161d024
Graden <101433010+GramGra07@users.noreply.github.com>
2025-02-18 22:32:47 -0700

    Update AutoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ggutil/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ggutil/AutoTest.java
@@ -11,6 +11,7 @@ import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.Heatseeker;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generators.EncoderSpecsBuilder;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.Encoder;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.EncoderStorage;
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.PathBuilder;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.Angle;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.AngleUnit;
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.Target2D;
@@ -27,26 +28,26 @@ public class AutoTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
         TwoWheelLocalizer localizer = new TwoWheelLocalizer(
                 hardwareMap,
-                new Target2D(0,0,new Angle(0, AngleUnit.RADIANS)),
-                new EncoderStorage(new Encoder(EncoderSpecsBuilder.INSTANCE.goBildaSwingArm(), "fl", DcMotorSimple.Direction.FORWARD,0.0,hardwareMap),null
-                        ,new Encoder(EncoderSpecsBuilder.INSTANCE.goBildaSwingArm(), "fr", DcMotorSimple.Direction.FORWARD,0.0,hardwareMap)),
+                new Target2D(0, 0, new Angle(0, AngleUnit.RADIANS)),
+                new EncoderStorage(new Encoder(EncoderSpecsBuilder.INSTANCE.goBildaSwingArm(), "fl", DcMotorSimple.Direction.FORWARD, 0.0, hardwareMap), null
+                        , new Encoder(EncoderSpecsBuilder.INSTANCE.goBildaSwingArm(), "fr", DcMotorSimple.Direction.FORWARD, 0.0, hardwareMap)),
                 new IMUParams("imu", new IMU.Parameters(orientationOnRobot))
         );
-        Driver driver = new Driver(this,"fl","fr","bl","br", DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD,localizer);
+        Driver driver = new Driver(this, "fl", "fr", "bl", "br", DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD, localizer);
         Heatseeker heatseeker = new Heatseeker(driver);
+        List<Waypoint> waypoints = new PathBuilder()
+                .addWaypoint(new Waypoint(new Target2D(0, 10.0, new Angle(0, AngleUnit.RADIANS)), 0.5))
+                .build();
 
-        List<Waypoint> waypoints = List.of(
-                new Waypoint(new Target2D(0,10.0,new Angle(0,AngleUnit.RADIANS)),0.5)
-        );
         waitForStart();
         if (opModeIsActive()) {
-            heatseeker.followPath(waypoints,1);
+            heatseeker.followPath(waypoints, 1);
         }
     }
 }
